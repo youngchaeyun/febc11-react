ac3a2bd0875c64046bf079dbac843b0bf2ee2a47
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-11-07 22:18:54 +0900

    강사님 코드 추가

---
+++ workspace-ins/ch01-buildup/counter/04/yong.js
@@ -0,0 +1,39 @@
+const yong = {
+  // 지정한 속성과 자식 노드를 가지는 요소 노드를 생성해서 반환
+  // <button type="button" onclick="handleUp()">+</button>
+  // createElement('button', { type: 'button', onclick: 'handleUp()' }, '+');
+  createElement: (tag, props, ...children) => {
+    // 요소 노드 생성
+    const elem = document.createElement(tag);
+
+    // 속성 추가
+    if(props){
+      for(const attrName in props){
+        elem.setAttribute(attrName, props[attrName]);
+      }
+    }
+
+    // 자식 노드 추가
+    for(let child of children){
+      if(typeof child === 'string' || typeof child === 'number'){
+        child = document.createTextNode(child);
+      }
+      elem.appendChild(child);
+    }
+
+    return elem;
+  },
+
+  // 루트노드를 관리하는 객체를 생성해서 반환
+  // createRoot(document.getElementById('root')).render(App);
+  createRoot: (rootNode) => {
+    return {
+      // 루트노드 하위에 지정한 함수를 실행해서 받은 컴포넌트를 렌더링 한다.
+      render(appFn){
+        rootNode.appendChild(appFn())
+      }
+    };
+  }
+};
+
+export default yong;
