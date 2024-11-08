1ba98954c837efda7becd47a2d3273769fe1f9be
Kilyong Jeong <uzoolove@gmail.com>
2024-11-08 10:30:02 +0900

    ✨ useState  추가

--- workspace-ins/ch01-buildup/counter/06/yong.js
+++ workspace-ins/ch01-buildup/counter/06/yong.js
@@ -37,10 +37,15 @@ const yong = (() => {
   // 루트노드를 관리하는 객체를 생성해서 반환
   // createRoot(document.getElementById('root')).render(App);
   const createRoot = (rootNode) => {
-    return {
+    let _appComponent;
+    return _root = {
       // 루트노드 하위에 지정한 함수를 실행해서 받은 컴포넌트를 렌더링 한다.
       render(appFn){
-        rootNode.appendChild(appFn())
+        _appComponent = _appComponent || appFn;
+        if(rootNode.firstChild){
+          rootNode.firstChild.remove();
+        }
+        rootNode.appendChild(_appComponent());
       }
     };
   };
@@ -48,10 +53,27 @@ const yong = (() => {
   // 상태값 관리
   // let [count, setCount] = Yong.useState(10);
   const useState = (initValue) => {
+    // 최초에 한번만 initValue 값으로 저장하고 useState가 다시 호출되면 initValue는 무시하고 저장된 값을 사용 
+    if(_stateValue === undefined){
+      // 최초 useState가 호출될때 한번만 실행
+      _stateValue = initValue;
+    }
 
+    // setValue(11);
+    function setValue(newValue){
+      const oldValue = _stateValue; // 10
+      _stateValue = newValue; // 11
+
+      // 두 값이 같은지 비교해서 같지 않을 경우에(상태가 변경된 경우) 리렌더링한다.
+      if(!Object.is(oldValue, newValue)){
+        _root.render();
+      }
+    }
+
+    return [_stateValue, setValue];
   };
 
-  return {};
+  return { createElement, createRoot, useState };
 })();
 
 export default yong;
