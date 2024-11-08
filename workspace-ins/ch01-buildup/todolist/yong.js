be5ef3c7e5f4867f9bf27dab3ddbdbef8433b2a4
Kilyong Jeong <uzoolove@gmail.com>
2024-11-08 15:02:33 +0900

    ✨ 상태 변경시 리렌더링

--- workspace-ins/ch01-buildup/todolist/yong.js
+++ workspace-ins/ch01-buildup/todolist/yong.js
@@ -67,8 +67,10 @@ const yong = (() => {
       const oldValue = _stateValue; // 10
       _stateValue = newValue; // 11
 
+      console.log('상태가 변경되었나?', oldValue, newValue);
       // 두 값이 같은지 비교해서 같지 않을 경우에(상태가 변경된 경우) 리렌더링한다.
       if(!Object.is(oldValue, newValue)){
+        console.log('리렌더링');
         _root.render();
       }
     }
