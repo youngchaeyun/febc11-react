826a746462ddd7fa1fdff532847e6fc50db23d68
Kilyong Jeong <uzoolove@gmail.com>
2024-11-08 15:52:02 +0900

    ✨ react 실습

--- workspace-ins/ch01-buildup/todolist/yong.js
+++ workspace-ins/ch01-buildup/todolist/yong.js
@@ -67,10 +67,13 @@ const yong = (() => {
       const oldValue = _stateValue; // 10
       _stateValue = newValue; // 11
 
-      console.log('상태가 변경되었나?', oldValue, newValue);
       // 두 값이 같은지 비교해서 같지 않을 경우에(상태가 변경된 경우) 리렌더링한다.
+      // 객체일때 같은 메모리 주소를 가지고 있으면 true
+      // 두 값이 모두 undefined 이거나 null 이면 true
+      // 두 값이 모두 true 이거나 false 이면 true
+      // String일 경우 두 값의 글자수, 순서, 모든 글자가 같으면 true
+      // Number 라면 같은 값을 가지고 있거나 둘다 NaN이면 true
       if(!Object.is(oldValue, newValue)){
-        console.log('리렌더링');
         _root.render();
       }
     }
