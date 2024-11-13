4ddca4168db94b1e34bcdf16220f2cea0ad2b845
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-11-13 11:27:23 +0900

    ✨ 2024년 11월 13일 react-ch02-start-props-src-components

---
+++ workspace/ch02-start/props/src/components/Button.jsx
@@ -0,0 +1,19 @@
+import "./Button.css";
+
+export default function Button({
+  children,
+  type = "button",
+  onClick: clickHandler,
+  color,
+}) {
+  return (
+    <button
+      className="rounded-button"
+      style={{ backgroundColor: color }}
+      type={type}
+      onClick={clickHandler}
+    >
+      {children}
+    </button>
+  );
+}
---
+++ workspace/ch02-start/props/src/components/Counter.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Button from "./Button";
+
+function Counter() {
+  let [count, setCount] = React.useState(0);
+
+  const handleDown = () => {
+    setCount(count - 1);
+  };
+  const handleUp = () => {
+    setCount(count + 1);
+  };
+  const handleReset = (event) => {
+    setCount(0);
+  };
+
+  return (
+    <div id="counter">
+      <Button onClick={handleDown}>-</Button>
+      <Button onClick={(event) => handleReset(event)}>0</Button>
+      <Button onClick={handleUp}>+</Button>
+      <span>{count}</span>
+    </div>
+  );
+}
+
+export default Counter;
