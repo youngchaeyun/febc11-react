fb6cdecb8164c4ead77924b23f2ee8643248799b
Kilyong Jeong <uzoolove@gmail.com>
2024-11-14 14:50:32 +0900

    🚧

--- workspace-ins/ch02-start/state/03/src/App.jsx
+++ workspace-ins/ch02-start/state/03/src/App.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
+
 function App(){
-  
+
+  const [position, setPosition] = useState({ x: 100, y: 250 });
+
   return (
     <>
       <h1>03 상태관리 대상이 객체일 경우 주의 사항</h1>
       <div
+        onPointerMove={ event => {
+          position.x = event.clientX;
+          position.y = event.clientY;
+        } }
         style={{
           position: 'relative',
           width: '100vw',
@@ -13,7 +21,7 @@ function App(){
           position: 'absolute',
           backgroundColor: 'red',
           borderRadius: '50%',
-          transform: `translate(${10}px, ${10}px)`,
+          transform: `translate(${position.x-10}px, ${position.y-10}px)`,
           left: -10,
           top: -10,
           width: 20,
