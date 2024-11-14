c1c920a10826cf0a61d4125b17248ea156759313
Kilyong Jeong <uzoolove@gmail.com>
2024-11-14 15:33:07 +0900

    🚧

--- workspace-ins/ch02-start/state/03/src/App.jsx
+++ workspace-ins/ch02-start/state/03/src/App.jsx
@@ -2,15 +2,22 @@ import { useState } from "react";
 
 function App(){
 
-  const [position, setPosition] = useState({ x: 100, y: 250 });
+  const [position, setPosition] = useState({ x: 50, y: 50 });
 
   return (
     <>
       <h1>03 상태관리 대상이 객체일 경우 주의 사항</h1>
       <div
         onPointerMove={ event => {
-          position.x = event.clientX;
-          position.y = event.clientY;
+          // 속성을 직접 수정하면 리렌더링되지 않음
+          // position.x = event.clientX;
+          // position.y = event.clientY;
+
+          const newPosition = { x: event.clientX, y: event.clientY - 80 };
+
+          setPosition(newPosition);
+
+          console.log(position);
         } }
         style={{
           position: 'relative',
