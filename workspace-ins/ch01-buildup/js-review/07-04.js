fbceafa5d07710bfefd0d63348d56d5c359cb0a0
Kilyong Jeong <uzoolove@gmail.com>
2024-11-14 10:23:10 +0900

    ✨

--- workspace-ins/ch01-buildup/js-review/07-04.js
+++ workspace-ins/ch01-buildup/js-review/07-04.js
@@ -1,16 +1,13 @@
-// Array.prototype.find(callback): 배열의 각 요소에 대해 콜백 함수를 실행
+// Array.prototype.filter(callback): 배열의 각 요소에 대해 콜백 함수를 실행
 // callback(elem, index)
-// true를 반환하는 첫번째 콜백 함수에 전달된 요소를 반환
-// true를 반환하는 콜백 함수가 없을 경우 undefined를 반환
+// true를 반환하는 콜백 함수에 전달된 요소를 배열로 모아서 반환
+// true를 반환하는 콜백 함수가 없을 경우 빈 배열을 반환
 
-// 배열 요소중 2와 3의 최소공배수 구하기
+// 배열 요소중 홀수의 합계 구하기
 var array = [6,7,8,1,4,5,2,3,9,10];
-// -를 반환할 경우 a, b 정렬
-// +를 반환할 경우 b, a 정렬
-// =을 반환할 경우 그대로 
-array.sort((a, b) => a - b);
-console.log(array);
 
-var result = array.find(num => num % 2 === 0 && num % 3 === 0);
+var result = 0;
 
-console.log(result); // 6
+array.filter(num => num % 2 !== 0).forEach(num => result += num);
+
+console.log(result); // 25
