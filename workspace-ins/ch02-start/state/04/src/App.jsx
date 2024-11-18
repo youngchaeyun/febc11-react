a3e06a7e7705a91dc4f178dc479c34f5e5d8e401
Kilyong Jeong <uzoolove@gmail.com>
2024-11-18 10:27:23 +0900

    🚧

--- workspace-ins/ch02-start/state/04/src/App.jsx
+++ workspace-ins/ch02-start/state/04/src/App.jsx
@@ -31,9 +31,39 @@ function App() {
   });
 
   const handleAddressChange = event => {
-    const address = user.extra.addressBook.find(address => address.id === Number(event.target.name));
-    address.value = event.target.value;
-    setUser({ ...user });
+    // 상태의 불변성이 지켜지지 않음
+    // const address = user.extra.addressBook.find(address => address.id === Number(event.target.name));
+    // address.value = event.target.value;
+    // const newState = { ...user };
+
+    // 상태의 불변성을 지키기 위해 복잡한 추가 작업 필요
+    const newAddressBook = user.extra.addressBook.map(address => {
+      if(address.id === Number(event.target.name)){
+        return { ...address, value: event.target.value };
+      }else{
+        return address;
+      }
+    });
+
+    const newState = {
+      ...user
+      , extra: {
+        ...user.extra,
+        addressBook: newAddressBook
+      }
+    };
+
+    // 회사 주소가 변경될 경우
+    console.log('user', user === newState); // false
+    console.log('user.extra', user.extra === newState.extra); // false
+    console.log('user.extra.addressBook', user.extra.addressBook === newState.extra.addressBook); // false
+    console.log('회사', user.extra.addressBook[0] === newState.extra.addressBook[0]); // false
+    console.log('집', user.extra.addressBook[1] === newState.extra.addressBook[1]); // true
+    console.log('회사 주소', user.extra.addressBook[0].value === newState.extra.addressBook[0].value); // false
+    console.log('집 주소', user.extra.addressBook[1].value === newState.extra.addressBook[1].value); // true
+    console.log('기존 회사 주소', user.extra.addressBook[0].value);
+
+    setUser(newState);
   };
 
   return (
