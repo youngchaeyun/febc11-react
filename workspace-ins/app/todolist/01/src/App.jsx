5565089ff43dcb8e5e5128d96874c28e044b533a
Kilyong Jeong <uzoolove@gmail.com>
2024-11-18 18:00:45 +0900

    🚧

--- workspace-ins/app/todolist/01/src/App.jsx
+++ workspace-ins/app/todolist/01/src/App.jsx
@@ -11,19 +11,35 @@ function App(){
     { _id: 3, title: '라면', done: true },
   ]);
 
+  // 할일 추가
   const addItem = (item) => {
+    // 데이터 갱신(상태 변경)
     const newItemList = [...itemList, item]; // 객체일 경우 새로운 객체로 만들어야 화면 갱신이 됨
     setItemList(newItemList); // setter를 이용해야 화면 갱신이 됨
+  };
+
+  // 할일 완료/미완료 처리
+  const toggleDone = (_id) => {
+    // 데이터 갱신(상태 변경)
+    const item = itemList.find(item => item._id === _id);
+    item.done = !item.done;
+    setItemList();
+  };
+
+  // 할일 삭제
+  const deleteItem = (_id) => {
+    // 데이터 갱신(상태 변경)
+    const newItemList = itemList.filter(item => item._id !== _id);
+    setItemList(newItemList);
   };
 
   return (
     <div id="todo">
       <Header />
-      <Todo itemList={ itemList } addItem={ addItem } />
+      <Todo itemList={ itemList } addItem={ addItem } toggleDone={ toggleDone } deleteItem={ deleteItem } />
       <Footer />
     </div>
   );
 }
-
 
 export default App
