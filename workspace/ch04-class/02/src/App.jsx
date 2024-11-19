790e3ace8ceb2e8e35c9af4861003211a4b337ae
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-11-19 17:01:42 +0900

    2024년 11월 19일 ch04-class-02

---
+++ workspace/ch04-class/02/src/App.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import ChildComponent from "./FunctionBase";
+
+class App extends Component {
+  render() {
+    return <ChildComponent />;
+  }
+}
+
+export default App;
