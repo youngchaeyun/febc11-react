dc579260abd6c5089aa46b4581a4d4d51422b3eb
Kilyong Jeong <uzoolove@gmail.com>
2024-11-19 14:42:47 +0900

    🚧

--- workspace-ins/ch04-class/02/src/FunctionBase.jsx
+++ workspace-ins/ch04-class/02/src/FunctionBase.jsx
@@ -1,51 +1,33 @@
-import { Component } from "react";
+import { useState } from "react";
 import PropTypes from 'prop-types';
 
-class ClickMe extends Component {
-  // state/setState는 부모에(Component) 정의되어 있는 이름
-  // 상태는 state 변수 하나만 사용 가능해서 여러개의 상태를 관리하려며 객체로 지정
-  // 함수형에서는 state 변수를 여러개 지정 가능(useState 훅)
-  // state = { count: 0 };
+function ClickMe({ level }) {
+  const [count, setCount] = useState(level || 1);
 
-  // 생성자 함수(props를 기준으로 state를 초기할 경우 등에서 작성)
-  constructor(props){
-    // 부모 클래스의 생성자를 통해 this가 초기화 되므로
-    // super()를 호출해야 자식 클래스에서 this를 사용할 수 있고
-    // super(props)를 호출해야 자식 클래스에서 this.props를 사용할 수 있고
-    super(props); // 필수로 작성
-    this.state = { count: props.level || 1 };
-  }
-
-  // arrow function으로 작성해야 this.state 등에 접근 가능
-  handleClick = () => {
-    this.setState({ count: this.state.count + (this.props.level || 1) });
+  const handleClick = () => {
+    setCount(count + (level || 1));
   };
 
-  render(){
-    return (
-      <div>
-        클릭 횟수 X { this.props.level || 1 }: { this.state.count }
-        <button onClick={ this.handleClick }>클릭</button>
-      </div>
-    );
-  }
+  return (
+    <div>
+      클릭 횟수 X { level || 1 }: { count }
+      <button onClick={ handleClick }>클릭</button>
+    </div>
+  );
 }
-
 ClickMe.propTypes = {
   level: PropTypes.number
 };
 
-class Parent extends Component {
-  render(){
-    return (
-      <div>
-        <h1>01 클래스 컴포넌트</h1>
-        <ClickMe level={2} />
-        <ClickMe level={5} />
-        <ClickMe />
-      </div>
-    );
-  }
+function Parent(){
+  return (
+    <div>
+      <h1>02 클래스 컴포넌트 - 함수 컴포넌트와 같이 사용</h1>
+      <ClickMe level={2} />
+      <ClickMe level={5} />
+      <ClickMe />
+    </div>
+  );
 }
 
 export default Parent;
---
+++ workspace-ins/ch04-class/03/src/LifeCycle.jsx
@@ -0,0 +1,76 @@
+import { Component } from "react";
+import PropTypes from 'prop-types';
+
+class ClickMe extends Component {
+
+  handleClick = () => {
+    this.setState({ count: this.state.count + (this.props.level || 1) });
+  };
+
+  // 1-1
+  constructor(props){
+    console.log('1-1 constructor 호출됨.');
+    super(props); // 필수로 작성
+    this.state = { count: props.level || 1 };
+  }
+
+  // 1-2/2-1
+  static getDerivedStateFromProps(props, state){
+    console.log('1-2/2-1 getDerivedStateFromProps 호출됨.');
+  }
+
+  // 2-2
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('2-2 shouldComponentUpdate 호출됨.');
+  }
+
+  // 1-3/2-3
+  render(){
+    console.log('1-3/2-3 render 호출됨.');
+    return (
+      <div>
+        클릭 횟수 X { this.props.level || 1 }: { this.state.count }
+        <button onClick={ this.handleClick }>클릭</button>
+      </div>
+    );
+  }
+
+  // 1-4
+  componentDidMount(){
+    console.log('1-4 componentDidMount 호출됨.');
+  }
+
+  // 2-4
+  getSnapshotBeforeUpdate(prevProps, prevState){
+    console.log('2-4 getSnapshotBeforeUpdate 호출됨.');
+  }
+
+  // 2-5
+  componentDidUpdate(){
+    console.log('2-5 componentDidUpdate 호출됨.');
+  }
+
+  // 3-1
+  componentWillUnmount(){
+    console.log('componentWillUnmount 호출됨.');
+  }
+}
+
+ClickMe.propTypes = {
+  level: PropTypes.number
+};
+
+class Parent extends Component {
+  render(){
+    return (
+      <div>
+        <h1>01 클래스 컴포넌트</h1>
+        <ClickMe level={2} />
+        <ClickMe level={5} />
+        <ClickMe />
+      </div>
+    );
+  }
+}
+
+export default Parent;
