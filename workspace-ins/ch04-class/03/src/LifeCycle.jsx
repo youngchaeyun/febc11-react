e0efb1ff562eeeafced42cf7dad40f7aac676e7f
Kilyong Jeong <uzoolove@gmail.com>
2024-11-19 15:44:09 +0900

    ✨

--- workspace-ins/ch04-class/03/src/LifeCycle.jsx
+++ workspace-ins/ch04-class/03/src/LifeCycle.jsx
@@ -17,14 +17,21 @@ class ClickMe extends Component {
   // 1-2/2-1
   static getDerivedStateFromProps(props, state){
     console.log('1-2/2-1 getDerivedStateFromProps 호출됨.');
+    console.log('  props', props);
+    console.log('  state', state);
 
-    return null;
+    if(state.count > props.level * 5){
+      return { count: 0 }; // 새로운 값으로 state 업데이트
+    }
+    return null; // state를 업데이트 하지 않음
   }
 
   // 2-2
   shouldComponentUpdate(nextProps, nextState){
     console.log('2-2 shouldComponentUpdate 호출됨.');
-    return true;
+    console.log('현재값', this.props, this.state);
+    console.log('다음값', nextProps, nextState);
+    return nextState.count % 2 === 0;
   }
 
   // 1-3/2-3
@@ -32,7 +39,7 @@ class ClickMe extends Component {
     console.log('1-3/2-3 render 호출됨.');
     return (
       <div>
-        클릭 횟수 X { this.props.level || 1 }: { this.state.count }
+        클릭 횟수 X { this.props.level || 1 }: { this.state.count % 2 === 0 }
         <button onClick={ this.handleClick }>클릭</button>
       </div>
     );
@@ -69,7 +76,7 @@ class Parent extends Component {
     return (
       <div>
         <h1>03 클래스 컴포넌트 - 컴포넌트의 라이프 사이클</h1>
-        <ClickMe level={2} />
+        <ClickMe level={1} />
       </div>
     );
   }
