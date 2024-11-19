b8bbb39c7267bef4dc956dac209351544a474116
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-11-19 14:41:58 +0900

    삭제한 파일-1

--- workspace/app/todolist/01/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer>
-      <p>멋쟁이 사자처럼 FrontEnd BootCamp</p>
-    </footer>
-  );
-}
-
-export default Footer;
--- workspace/app/todolist/01/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-  return (
-    <header>
-      <h1>Todo List - 기능 추가 :()</h1>
-    </header>
-  );
-}
-
-export default Header;
