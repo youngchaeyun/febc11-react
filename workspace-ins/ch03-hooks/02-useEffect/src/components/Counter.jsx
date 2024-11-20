35bed309f9032995bc6b3d114c221442731b641f
Kilyong Jeong <uzoolove@gmail.com>
2024-11-20 10:51:29 +0900

    🚧

--- workspace-ins/ch03-hooks/02-useEffect/src/components/Counter.jsx
+++ workspace-ins/ch03-hooks/02-useEffect/src/components/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "./Button";
 import PropTypes from "prop-types";
 
@@ -7,6 +7,9 @@ Counter.propTypes = {
 };
 
 function Counter({ children = '0' }){
+  
+  console.log('Counter 렌더링');
+
   const initCount = Number(children);
   const [count, setCount] = useState(initCount);
   const [step, setStep] = useState(1);
@@ -14,12 +17,51 @@ function Counter({ children = '0' }){
   const handleDown = () => {
     setCount(count - step);
   };
+
   const handleUp = () => {
     setCount(count + step);
   };
+
   const handleReset = event => {
     setCount(initCount);
   };
+
+  // 1초 후에 자동으로 값 한번 증가
+  // 시키고 싶었는데 무한 렌더링 발생
+  // setTimeout(() => {
+  //   handleUp();
+  // }, 1000);
+
+  // 마운트 된 후에 값 증가
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     handleUp();
+  //   }, 1000);
+  //   console.log('dependencies에 빈배열을 지정하면 컴포넌트가 마운트된 후에 한번만 호출됨(업데이트 후에는 호출되지 않음)');
+  // }, []);
+
+  // 마운트 된 후와 업데이트 된 후에 값 증가
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     handleUp();
+  //   }, 1000);
+  //   console.log('dependencies를 지정하지 않으면 컴포넌트가 마운트된 후와 업데이트 된 후에 호출됨');
+  // });
+
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     handleUp();
+  //   }, 1000);
+  //   console.log('dependencies에 값을 지정하면 컴포넌트가 업데이트 될때 지정한 값중 하나라도 수정되었을 경우 호출됨');
+  // }, [step]);
+
+  useEffect(() => {
+    console.log('setup 함수 호출.');
+    setInterval(() => {
+      console.log(step, new Date());
+    }, 1000);
+  }, []);
 
   return (
     <div id="counter">
