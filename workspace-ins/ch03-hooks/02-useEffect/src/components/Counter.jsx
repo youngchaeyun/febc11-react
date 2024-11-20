5d68ecb3af49da8c04fe6497f67612159a90c6cf
Kilyong Jeong <uzoolove@gmail.com>
2024-11-20 15:26:13 +0900

    📝

--- workspace-ins/ch03-hooks/02-useEffect/src/components/Counter.jsx
+++ workspace-ins/ch03-hooks/02-useEffect/src/components/Counter.jsx
@@ -81,7 +81,7 @@ function Counter({ children = '0' }){
   return (
     <div id="counter">
       <label htmlFor="step">증감치</label>
-      {/* 제어 컴포넌트 value, onClick 사용 */}
+      {/* 제어 컴포넌트 value, onChange 사용 */}
       <input id="step" type="number" style={{ width: '40px' }} value={ step } 
         onChange={ e => setStep(Number(e.target.value)) } />
       <Button color="red" onClick={ handleDown }>-</Button>
