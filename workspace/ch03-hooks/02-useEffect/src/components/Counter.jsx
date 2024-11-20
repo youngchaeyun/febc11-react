f6abddb515c0a5103751c9ee3710ae1c8e97a4f9
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-11-20 10:44:41 +0900

    🚧 ✨ 2024년 11월 20일 ch03-hooks-02-useEffect-components (파일 추가 & 작성 중)

---
+++ workspace/ch03-hooks/02-useEffect/src/components/Counter.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import Button from "./Button";
+import PropTypes from "prop-types";
+
+Counter.propTypes = {
+  children: PropTypes.string,
+};
+
+function Counter({ children = "0" }) {
+  const initCount = Number(children);
+  const [count, setCount] = useState(initCount);
+  const [step, setStep] = useState(1);
+
+  const handleDown = () => {
+    setCount(count - step);
+  };
+  const handleUp = () => {
+    setCount(count + step);
+  };
+  const handleReset = (event) => {
+    setCount(initCount);
+  };
+
+  return (
+    <div id="counter">
+      <label htmlFor="step">증감치</label>
+      {/* 제어 컴포넌트 value, onClick 사용 */}
+      <input
+        id="step"
+        type="number"
+        style={{ width: "40px" }}
+        value={step}
+        onChange={(e) => setStep(Number(e.target.value))}
+      />
+      <Button color="red" onClick={handleDown}>
+        -
+      </Button>
+      <Button onClick={handleReset}>{initCount}</Button>
+      <Button color="blue" onClick={handleUp}>
+        +
+      </Button>
+      <span>{count}</span>
+    </div>
+  );
+}
+
+export default Counter;
---
+++ workspace/ch03-hooks/02-useEffect/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h1>02 useEffect - side effect 관리</h1>
+    </header>
+  );
+}
+
+export default Header;
