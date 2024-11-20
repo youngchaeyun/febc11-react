1aa7b09710d8c21fdce068774a8793a35db2e45f
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-11-20 10:07:13 +0900

    ✨ 2024년 11월 19일 ch04-class-03 (03 클래스 컴포넌트 - 컴포넌트의 라이프 사이클) (경고가 표시된 파일이 있음-해결 못함.)

---
+++ workspace/ch04-class/03/src/App.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import ChildComponent from "./LifeCycle";
+
+class App extends Component {
+  render() {
+    return <ChildComponent />;
+  }
+}
+
+export default App;
