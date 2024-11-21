f2a5443c991421717625f2d45c01094b1ea82c51
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-11-21 10:35:12 +0900

    ✨ 2024년 11월 20일 ch03-hooks-02-useEffect-src-components (Counter.jsx 수정)

--- workspace/ch03-hooks/02-useEffect/src/components/Counter.jsx
+++ workspace/ch03-hooks/02-useEffect/src/components/Counter.jsx
@@ -44,7 +44,7 @@ function Counter({ children = "0" }) {
   //   setTimeout(() => {
   //     handleUp();
   //   }, 1000);
-  //   console.log('dependencies를 지정하지 않으면 컴포넌트가 마운트된 후와 업데이트 된 후에 호출됨');
+  //   console.log('dependencies를 지정하지 않으면 컴포넌트가 마운트 된 후와 업데이트 된 후에 호출됨');
   // });
 
   // useEffect(() => {
@@ -55,13 +55,14 @@ function Counter({ children = "0" }) {
   // }, [step]);
 
   useEffect(() => {
+    // setup
     console.log("setup 함수 호출.");
     const timer = setInterval(() => {
       console.log(new Date());
     }, 1000);
     return () => {
       clearInterval(timer);
-    };
+    }; // cleanup
   }, []);
 
   // useEffect(() => {
@@ -78,7 +79,7 @@ function Counter({ children = "0" }) {
   return (
     <div id="counter">
       <label htmlFor="step">증감치</label>
-      {/* 제어 컴포넌트 value, onClick 사용 */}
+      {/* 제어 컴포넌트 value, onChange 사용 */}
       <input
         id="step"
         type="number"
