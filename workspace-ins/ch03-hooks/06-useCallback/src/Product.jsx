8e30adba9f856838b749ccc5abef23c082761817
Kilyong Jeong <uzoolove@gmail.com>
2024-11-21 11:16:22 +0900

    🚧

--- workspace-ins/ch03-hooks/06-useCallback/src/Product.jsx
+++ workspace-ins/ch03-hooks/06-useCallback/src/Product.jsx
@@ -1,13 +1,22 @@
-function Product(){
+import PropTypes from "prop-types";
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  mainImage: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+};
+
+function Product({ name, price, mainImage, content }){
   return (
     <>
       <h2>상품 설명</h2>
-      <p>상품명: 나이키 에어 맥스</p>
-      <p>가격: 131,000원</p>
+      <p>상품명: { name }</p>
+      <p>가격: { price.toLocaleString() }원</p>
       <p>상품 설명</p>
       <div>
-        <img src="https://11.fesp.shop/files/00-nike/AIR_MAX_A_01.png" width="600" />
-        <p>나이키 에어 맥스는 어쩌고 저쩌고...</p>
+        <img src={ `https://11.fesp.shop${ mainImage }` } width="600" />
+        <p>{ content }</p>
       </div>
     </>
   );
