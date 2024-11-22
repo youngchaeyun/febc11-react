0a821eb29c0fffb9e8b34309e5e0a9e10d91e970
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-11-22 15:09:34 +0900

    🚧 ✨ 2024년 11월 22일 ch05-router-02-src 파일들 업로드 - 작성중

---
+++ workspace/ch05-router/02/src/Page2.jsx
@@ -0,0 +1,9 @@
+function Page2() {
+  return (
+    <>
+      <h2>Page2</h2>
+    </>
+  );
+}
+
+export default Page2;
---
+++ workspace/ch05-router/02/src/routes.jsx
@@ -0,0 +1,40 @@
+import Layout from "./Layout";
+import Home from "./Home";
+import Page1 from "./Page1";
+import Page2 from "./Page2";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter(
+  [
+    {
+      //   path: '/',
+      //   element: <Home />
+      // }, {
+      //   path: '/page1',
+      //   element: <Page1 />
+      // }, {
+      //   path: '/page2',
+      //   element: <Page2 />
+      // }, {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "page1", element: <Page1 /> },
+        { path: "page2", element: <Page2 /> },
+      ],
+    },
+  ],
+  {
+    future: {
+      // 없으면 콘솔에 경고 표시
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_relativeSplatPath: true,
+      v7_skipActionErrorRevalidation: true,
+    },
+  }
+);
+
+export default router;
