7732b1edfc16c8397ebb37bbe53665f911a53b66
Kilyong Jeong <uzoolove@gmail.com>
2024-11-22 15:14:59 +0900

    ✨

--- workspace-ins/ch05-router/02/src/Header.jsx
+++ workspace-ins/ch05-router/02/src/Header.jsx
@@ -1,15 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 function Header() {
   return (
     <>
       <header>
-        <h1>리액트 라우터</h1>
-        <Link to="/">home</Link>
+        <h1>리액트 라우터 - 02 리액트 라우터 사용</h1>
+
+        <NavLink className={ ({ isActive }) => isActive ? 'menu-dark' : 'menu' } to="/home">Home</NavLink>
+        <NavLink className={ ({ isActive }) => isActive ? 'menu-dark' : 'menu' } to="/page1">page1</NavLink>
+        <NavLink className={ ({ isActive }) => isActive ? 'menu-dark' : 'menu' } to="/page2">page2</NavLink>
+
+
+        {/* <Link to="/">home</Link>
         <br/>
         <Link to="/page1">page1</Link>
         <br/>
-        <Link to="/page2">page2</Link>
+        <Link to="/page2">page2</Link> */}
       </header>
     </>
   );
--- workspace-ins/ch05-router/02/src/routes.jsx
+++ workspace-ins/ch05-router/02/src/routes.jsx
@@ -2,7 +2,7 @@ import Layout from './Layout';
 import Home from './Home';
 import Page1 from './Page1';
 import Page2 from './Page2';
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Navigate } from 'react-router-dom';
 
 const router = createBrowserRouter([{
   //   path: '/',
@@ -17,7 +17,8 @@ const router = createBrowserRouter([{
     path: '/',
     element: <Layout />,
     children: [
-      { index: true, element: <Home /> },
+      { index: true, element: <Navigate to="/home" /> },
+      { path: 'home', element: <Home /> },
       { path: 'page1', element: <Page1 /> },
       { path: 'page2', element: <Page2 /> },
     ],
