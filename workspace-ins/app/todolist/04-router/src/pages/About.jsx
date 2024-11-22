bb10ab5413c93f93b410f8503a30dcd1b577d4b7
Kilyong Jeong <uzoolove@gmail.com>
2024-11-22 15:49:39 +0900

    🚧

--- workspace-ins/app/todolist/04-router/src/pages/About.jsx
+++ workspace-ins/app/todolist/04-router/src/pages/About.jsx
@@ -1,8 +1,11 @@
 function About() {
   return (
-    <>
-
-    </>
+    <div id="main">
+      <h2>About</h2>
+      <div className="todo">
+        <p>Front-End BootCamp Todo List 라우팅 적용 실습</p>
+      </div>
+    </div>
   );
 }
 
