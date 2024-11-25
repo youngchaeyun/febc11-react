7ebad15f5415705c038b07678eccd1e9c7ed564a
Kilyong Jeong <uzoolove@gmail.com>
2024-11-25 13:09:12 +0900

    ✨

--- workspace-ins/app/todolist/04-router/src/App.jsx
+++ workspace-ins/app/todolist/04-router/src/App.jsx
@@ -1,10 +1,13 @@
 import { RouterProvider } from "react-router-dom";
-import router from "./routes";
-// import router from "./routes-lazy";
+// import router from "./routes";
+import router from "./routes-lazy";
+import { Suspense } from "react";
 
 function App() {
   return (
-    <RouterProvider router={router} future={{ v7_startTransition: true }} />
+    <Suspense fallback={ <div>Loading...</div> }>
+      <RouterProvider router={router} future={{ v7_startTransition: true }} />
+    </Suspense>
   );
 }
 
