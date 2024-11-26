31e1340908a8bc8901a8bc36ce075ad87e321b72
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-11-26 10:32:47 +0900

    🚧 ✨ 2024년 11월 26일 app-todolist-04-router-src-pages (TodoList.jsx 파일 수정과 TodoListItem.jsx 파일 새로 추가함)

--- workspace/app/todolist/04-router/src/pages/TodoList.jsx
+++ workspace/app/todolist/04-router/src/pages/TodoList.jsx
@@ -1,36 +1,46 @@
-import { Link } from "react-router-dom";
+import TodoListItem from "@pages/TodoListItem";
+import { useEffect, useState } from "react";
+import { Link, Outlet } from "react-router-dom";
+
+const dummyData = {
+  items: [
+    {
+      _id: 1,
+      title: "잠자기",
+    },
+    {
+      _id: 2,
+      title: "자바스크립트 복습",
+      done: true,
+    },
+  ],
+};
 
 function TodoList() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    setData(dummyData);
+  }, []); // 마운트된 후에 한번만 호출
+
+  const itemList = data?.items.map((item) => (
+    <TodoListItem key={item._id} item={item} />
+  ));
+
   return (
     <div id="main">
       <h2>할일 목록</h2>
       <div className="todo">
-        <Link to="/add">추가</Link>
+        <Link to="/list/add">추가</Link>
         <br />
         <form className="search">
           <input type="text" autoFocus />
           <button type="submit">검색</button>
         </form>
-        <ul className="todolist">
-          <li>
-            <span>1</span>
-            <Link to="/list/1">잠자기</Link>
-            <Link to="/list">삭제</Link>
-          </li>
-          <li>
-            <span>2</span>
-            <Link to="/list/2">자바스크립트 복습</Link>
-            <Link to="/list">삭제</Link>
-          </li>
-          <li>
-            <span>3</span>
-            <Link to="/list/3">
-              <s>리액트 과제 하기</s>
-            </Link>
-            <Link to="/list">삭제</Link>
-          </li>
-        </ul>
+        <ul className="todolist">{itemList}</ul>
       </div>
+
+      <Outlet />
     </div>
   );
 }
