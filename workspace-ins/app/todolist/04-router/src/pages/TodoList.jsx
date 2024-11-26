f151225408c265ca39e5f300c8f8dd0de4b9bd7c
Kilyong Jeong <uzoolove@gmail.com>
2024-11-26 10:05:46 +0900

    🚧

--- workspace-ins/app/todolist/04-router/src/pages/TodoList.jsx
+++ workspace-ins/app/todolist/04-router/src/pages/TodoList.jsx
@@ -9,6 +9,7 @@ const dummyData = {
   }, {
     _id: 2,
     title: '자바스크립트 복습',
+    done: true,
   }]
 };
 
@@ -33,21 +34,7 @@ function TodoList() {
           <button type="submit">검색</button>
         </form>
         <ul className="todolist">
-          <li>
-            <span>1</span>
-            <Link to="/list/1">잠자기</Link>
-            <Link to="/list">삭제</Link>
-          </li>
-          <li>
-            <span>2</span>
-            <Link to="/list/2">자바스크립트 복습</Link>
-            <Link to="/list">삭제</Link>
-          </li>
-          <li>
-            <span>3</span>
-            <Link to="/list/3"><s>리액트 과제 하기</s></Link>
-            <Link to="/list">삭제</Link>
-          </li>
+          { itemList }
         </ul>
       </div>
 
--- workspace-ins/app/todolist/04-router/src/pages/TodoListItem.jsx
+++ workspace-ins/app/todolist/04-router/src/pages/TodoListItem.jsx
@@ -5,7 +5,7 @@ TodoListItem.propTypes = {
   item: PropTypes.shape({
     _id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
-    done: PropTypes.bool.isRequired,
+    done: PropTypes.bool,
   }),
 };
 
@@ -13,7 +13,7 @@ function TodoListItem({ item }) {
   return (
     <li>
       <span>{ item._id }</span>
-      <Link to={`/list/${ item._id }`}>{ item.done ? <s>${ item.title }</s> : item.title }</Link>
+      <Link to={`/list/${ item._id }`}>{ item.done ? <s>{ item.title }</s> : item.title }</Link>
       <Link to="/list">삭제</Link>
     </li>
   );
