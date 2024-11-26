9f4305c1f01056d492060db2f8903fa7ae98f275
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-11-26 15:17:25 +0900

    일단 05-api 폴더 전체 작업 전 업로드

---
+++ workspace/app/todolist/05-api/src/pages/TodoAdd.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+function TodoAdd() {
+  return (
+    <div id="main">
+      <h2>할일 추가</h2>
+      <div className="todo">
+        <form>
+          <label htmlFor="title">제목 :</label>
+          <input type="text" id="title" autoFocus />
+          <br />
+          <label htmlFor="content">내용 :</label>
+          <textarea id="content" cols="23" rows="5"></textarea>
+          <br />
+          <Link to="/list/1">추가</Link>
+          <Link to="/list">취소</Link>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default TodoAdd;
