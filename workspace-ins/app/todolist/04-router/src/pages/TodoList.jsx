39f7cdc878068b52ee59dd47bbfa022627d02c08
Kilyong Jeong <uzoolove@gmail.com>
2024-11-26 09:50:54 +0900

    🚧

--- workspace-ins/app/todolist/04-router/src/pages/TodoList.jsx
+++ workspace-ins/app/todolist/04-router/src/pages/TodoList.jsx
@@ -1,6 +1,27 @@
+import TodoListItem from "@pages/TodoListItem";
+import { useEffect, useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 
+const dummyData = {
+  items: [{
+    _id: 1,
+    title: '잠자기',
+  }, {
+    _id: 2,
+    title: '자바스크립트 복습',
+  }]
+};
+
 function TodoList() {
+
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    setData(dummyData);
+  }, []); // 마운트된 후에 한번만 호출
+
+  const itemList = data?.items.map(item => <TodoListItem key={ item._id } item={ item } />);
+
   return (
     <div id="main">
       <h2>할일 목록</h2>
---
+++ workspace-ins/app/todolist/04-router/src/pages/TodoListItem.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+TodoListItem.propTypes = {
+  item: PropTypes.shape({
+    _id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    done: PropTypes.bool.isRequired,
+  }),
+};
+
+function TodoListItem({ item }) {
+  return (
+    <li>
+      <span>{ item._id }</span>
+      <Link to={`/list/${ item._id }`}>{ item.done ? <s>${ item.title }</s> : item.title }</Link>
+      <Link to="/list">삭제</Link>
+    </li>
+  );
+};
+
+export default TodoListItem;
