34b54c58973a76932260299166ca7bd1cad9103e
Kilyong Jeong <uzoolove@gmail.com>
2024-11-27 15:37:01 +0900

    🧪

--- workspace-ins/ch06-contextapi/after/src/components/Left2.jsx
+++ workspace-ins/ch06-contextapi/after/src/components/Left2.jsx
@@ -1,17 +1,17 @@
 import Left3 from '@components/Left3';
-import { SimpleContext } from '@context/SimpleContext';
-import { useContext, useEffect } from 'react';
+import { useEffect } from 'react';
 
 function Left2() {
   useEffect(()=>{
     console.log('    # Left2 렌더링.');
   });
 
-  const simple = useContext(SimpleContext);
+  // undefined 반환
+  // const simple = useContext(SimpleContext);
 
   return (
     <div>
-      <h2>Left2 - { simple.hello }</h2>
+      <h2>Left2</h2>
       <Left3 />
     </div>
   );
