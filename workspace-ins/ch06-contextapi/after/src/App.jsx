13f1e1e996888803e7acc43cbca5cfa8cc09a47b
Kilyong Jeong <uzoolove@gmail.com>
2024-11-27 14:51:05 +0900

    🚧

--- workspace-ins/ch06-contextapi/after/src/App.jsx
+++ workspace-ins/ch06-contextapi/after/src/App.jsx
@@ -1,13 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import Left1 from '@components/Left1';
 import Right1 from '@components/Right1';
+import { CounterProvider } from '@context/CounterContext';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const countUp = function(step){
-    setCount(count + step);
-  };
 
   useEffect(()=>{
     console.log('# App 렌더링.');
@@ -19,8 +15,10 @@ function App() {
       <div id="container">
         <h1>App</h1>
         <div id="grid">
-          <Left1 />
-          <Right1 />
+          <CounterProvider>
+            <Left1 />
+            <Right1 />
+          </CounterProvider>
         </div>
       </div>
     </>
---
+++ workspace-ins/ch06-contextapi/after/src/context/CounterContext.jsx
@@ -0,0 +1,51 @@
+import PropTypes from "prop-types";
+import { createContext, useState } from "react";
+
+// 1. Context 객체 생성
+const CounterContext = createContext();
+
+CounterProvider.propTypes = {
+  children: PropTypes.node
+};
+
+// 3. Provider 컴포넌트를 작성해서 export
+export function CounterProvider({ children }) {
+  // 4. 데이터, 상태, 상태를 관리하는 함수 정의
+  // Left3에 전달
+  const [count, setCount] = useState(10);
+
+  // Right3에 전달
+  const countDown = function(step){
+    setCount(count - step);
+  };
+  const reset = function(){
+    setCount(0);
+  };
+  const countUp = function(step){
+    setCount(count + step);
+  };
+
+  const values = {
+    state: { count },
+    actions: { countDown, reset, countUp },
+  };
+
+  // 5. Context 객체의 Provider로 자식 컴포넌트를 감싸서 리턴
+  // value 속성에 전달할 Context 값 지정
+  return (
+    <CounterContext.Provider value={ values }>
+      { children }
+    </CounterContext.Provider>
+  );
+}
+
+{/* <CounterProvider>
+  <App />
+</CounterProvider> */}
+
+{/* <CounterContext.Provider value={ values }>
+  <App />
+</CounterContext.Provider> */}
+
+// 2. Context 객체 export
+export default CounterContext;
