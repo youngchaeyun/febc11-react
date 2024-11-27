2c155bb1104f8eaa94d351aa06350db681612a58
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-11-27 13:43:05 +0900

    ✅ ✨ 2024년 11월 27일 app-todolist-05-api-src-pages 완료

--- workspace/app/todolist/05-api/src/pages/TodoDetail.jsx
+++ workspace/app/todolist/05-api/src/pages/TodoDetail.jsx
@@ -1,16 +1,18 @@
+import useAxios from "@hooks/useAxios";
+import useAxiosInstance from "@hooks/useAxiosInstance";
 import { useEffect, useState } from "react";
-import { Link, Outlet, useOutletContext, useParams } from "react-router-dom";
+import { Link, Outlet, useNavigate, useParams } from "react-router-dom";
 
-const dummyData = {
-  item: {
-    _id: 5,
-    title: "Javascript 공부",
-    content: "열심히 하자",
-    done: false,
-    createdAt: "2024.11.21 16:49:00",
-    updatedAt: "2024.11.21 16:49:00",
-  },
-};
+// const dummyData = {
+//   item: {
+//     "_id": 5,
+//     "title": "Javascript 공부",
+//     "content": "열심히 하자",
+//     "done": false,
+//     "createdAt": "2024.11.21 16:49:00",
+//     "updatedAt": "2024.11.21 16:49:00"
+//   }
+// };
 
 function TodoDetail() {
   // URL의 파라미터 추출
@@ -19,13 +21,27 @@ function TodoDetail() {
   const { _id } = useParams();
   console.log(_id);
 
+  const navigate = useNavigate();
+
   const [data, setData] = useState();
+  // useEffect(() => {
+  //   // TODO: API 서버 통신
+
+  //   setData(dummyData);
+  // }, []);
+
+  // const { data } = useAxios({ url: `/todolist/${_id}` });
+
+  const axios = useAxiosInstance();
+  // API 서버에서 상세정보를 조회
+  const fetchDetail = async () => {
+    const res = await axios.get(`/todolist/${_id}`);
+    setData(res.data);
+  };
 
   useEffect(() => {
-    // TODO: API 서버 통신
-
-    setData(dummyData);
-  }, []);
+    fetchDetail();
+  }, []); // 마우트될 때 한번만 호출
 
   return (
     <div id="main">
@@ -41,10 +57,12 @@ function TodoDetail() {
             <div>수정일 : {data.item.updatedAt}</div>
 
             <Link to="./edit">수정</Link>
-            <Link to="/list">목록</Link>
+            <button type="button" onClick={() => navigate(-1)}>
+              목록
+            </button>
           </div>
 
-          <Outlet context={{ item: data.item }} />
+          <Outlet context={{ item: data.item, refetch: fetchDetail }} />
         </>
       )}
     </div>
