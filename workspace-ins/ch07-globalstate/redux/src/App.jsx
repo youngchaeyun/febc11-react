79f5732cdf7b01abc63a7436705ce45e8e0ab492
Kilyong Jeong <uzoolove@gmail.com>
2024-11-28 11:37:05 +0900

    🚧

---
+++ workspace-ins/ch07-globalstate/redux/src/App.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import Left1 from '@components/Left1';
+import Right1 from '@components/Right1';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const countUp = function(step){
+    setCount(count + step);
+  };
+
+  useEffect(()=>{
+    console.log('# App 렌더링.');
+  });
+
+  return (
+    <>
+      <h1>Context API - Sample</h1>
+      <div id="container">
+        <h1>App</h1>
+        <div id="grid">
+          <Left1 />
+          <Right1 />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
