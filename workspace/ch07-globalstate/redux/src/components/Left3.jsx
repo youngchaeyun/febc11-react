c2d42c301385747b261e896a8f8db2c29e38a5a8
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-11-28 13:50:03 +0900

    ✨ ch07-globalstate-redux-src-components 작성 완료!

---
+++ workspace/ch07-globalstate/redux/src/components/Left3.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+
+function Left3() {
+  useEffect(() => {
+    console.log("      # Left3 렌더링.");
+  });
+
+  // redux
+  const count = useSelector((state) => state.count);
+
+  return (
+    <div>
+      <h3>Left3</h3>
+      <span>{count}</span>
+    </div>
+  );
+}
+
+export default Left3;
---
+++ workspace/ch07-globalstate/redux/src/components/Right3.jsx
@@ -0,0 +1,30 @@
+import counterActionCreator from "@redux/counterActionCreator";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+function Right3() {
+  useEffect(() => {
+    console.log("      # Right3 렌더링.");
+  });
+
+  // 상태값 수정
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h3>Right3</h3>
+      {/* <button onClick={ () => dispatch({ type: 'countUp', payload: { step: 2 } }) }> +2 </button> */}
+      <button onClick={() => dispatch(counterActionCreator.countDown(1))}>
+        -1
+      </button>
+      <button onClick={() => dispatch(counterActionCreator.countDown(1))}>
+        0
+      </button>
+      <button onClick={() => dispatch(counterActionCreator.countDown(1))}>
+        +2
+      </button>
+    </div>
+  );
+}
+
+export default Right3;
