c102d77db73dc8a44ea0631343fc7ea0d59849d8
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-12-02 11:19:21 +0900

    ✨ 2024년 11월 28일 ch07-globalstate-zustand-src 파일 수정함.

--- workspace/ch07-globalstate/zustand/src/App.jsx
+++ workspace/ch07-globalstate/zustand/src/App.jsx
@@ -3,19 +3,13 @@ import Left1 from "@components/Left1";
 import Right1 from "@components/Right1";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const countUp = function (step) {
-    setCount(count + step);
-  };
-
   useEffect(() => {
     console.log("# App 렌더링.");
   });
 
   return (
     <>
-      <h1>Context API - Sample</h1>
+      <h1>전역 상태 관리 - Zustand</h1>
       <div id="container">
         <h1>App</h1>
         <div id="grid">
