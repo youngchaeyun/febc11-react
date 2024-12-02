f6a3ff78f6a2a5127448d45a49708ef0e080ae37
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-12-02 11:24:22 +0900

    ✨ 2024년 11월 28일 ch07-globalstate-zustand-src-zustand (zustand 폴더와 counter.js 파일 추가함.)

---
+++ workspace/ch07-globalstate/zustand/src/zustand/counter.js
@@ -0,0 +1,12 @@
+import { create } from "zustand";
+
+const useCounterState = create((set, get) => ({
+  count: 6,
+
+  countUp: (step) => {
+    const newState = { count: get().count + step };
+    set(newState);
+  },
+}));
+
+export default useCounterState;
