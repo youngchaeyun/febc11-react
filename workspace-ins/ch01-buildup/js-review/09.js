777a7c6ce5724a820ce650e5a2532794acd50b9a
Kilyong Jeong <uzoolove@gmail.com>
2024-12-02 14:52:36 +0900

    🚧

--- workspace-ins/ch01-buildup/js-review/09.js
+++ workspace-ins/ch01-buildup/js-review/09.js
@@ -19,3 +19,13 @@ console.log(result);
 const result2 = sayHello(['안녕하세요. ', '님. 오늘 날씨는 ', ' 입니다. 즐거운 ', ' 보내세요.'], '데이지', '흐림', '하루');
 // => 안녕하세요. <strong>데이지</strong>님. 오늘 날씨는 <strong>흐림</strong> 입니다. 즐거운 <strong>하루</strong> 보내세요.
 console.log(result2);
+
+const userName = '무지';
+const weather = '맑음';
+const str = `안녕하세요. ${userName}님. 오늘 날씨는 ${weather} 입니다.`;
+
+const result3 = sayHello(['안녕하세요. ', '님. 오늘 날씨는 ', ' 입니다.'], userName, weather);
+
+// Tagged Template
+const result4 = sayHello`안녕하세요. ${userName}님. 오늘 날씨는 ${weather} 입니다.`;
+console.log('result4', result4);
