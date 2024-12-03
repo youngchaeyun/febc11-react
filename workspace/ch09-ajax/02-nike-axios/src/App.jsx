2c9543d2213612645bc7ef0ac29dafc6a606a276
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-12-03 16:44:24 +0900

    ✨ 2024년 12월 03일 ch09-ajax-02-nike-axios-src (axios 로 수정)

--- workspace/ch09-ajax/02-nike-axios/src/App.jsx
+++ workspace/ch09-ajax/02-nike-axios/src/App.jsx
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useState } from "react";
 import Product from "./Product";
 import Shipping from "./Shipping";
 import { DotLoader } from "react-spinners";
+import axios from "axios";
 
 function App() {
   const [data, setData] = useState(); // 1(마운트)
@@ -11,8 +12,8 @@ function App() {
   const fetchData = async (_id) => {
     setIsLoading(true);
     try {
-      const res = await fetch(
-        `https://11.fesp.shop/producsdfsdfsdfts/${_id}?delay=3000`,
+      const res = await axios(
+        `https://11.fesp.shop/products/${_id}?delay=3000`,
         {
           headers: {
             "client-id": "00-nike",
@@ -20,19 +21,10 @@ function App() {
         }
       );
       console.log("res", res);
-      const jsonData = await res.json();
-      console.log("jsonData", jsonData);
-
-      if (res.ok) {
-        setData(jsonData.item); // 4번(마운트 후)
-        setError(null);
-      } else {
-        // 4xx, 5xx 응답 상태코드를 받을 경우
-        setError(jsonData);
-        setData(null);
-      }
+      setData(res.data.item); // 4번 (마운트 후)
+      setError(null);
     } catch (err) {
-      // network 에러
+      // network 에러, 4xx, 5xx 응답일 경우
       console.error(err);
       setError({ message: "잠시 후 다시 요청하세요." });
       setData(null);
@@ -42,7 +34,7 @@ function App() {
   };
 
   useEffect(() => {
-    fetchData(4); // 3번(마운트 후)
+    fetchData(7); // 3번(마운트 후)
   }, []); // 마운트 된 이후에 최초 한번만 실행
 
   const basicShippingFees = 3000;
@@ -64,7 +56,7 @@ function App() {
   // return <h1></h1> // 2번(마운트)
   return (
     <>
-      <h1>01 Nike 상품 상세 조회 - Fetch API</h1>
+      <h1>02 Nike 상품 상세 조회 - Axios</h1>
       {isLoading && <DotLoader />}
       {error && <p>{error.message}</p>}
       {data && (
