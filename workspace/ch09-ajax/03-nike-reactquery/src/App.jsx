fdd5886e108f0af0dc1eaaf9e12d0fa9676976fc
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-12-04 15:32:43 +0900

    🚧 2024년 12월 04일 ch09-ajax-03-nike-reactquery-src (상품 구매에 필요한 useMutation 사용 중..)

--- workspace/ch09-ajax/03-nike-reactquery/src/App.jsx
+++ workspace/ch09-ajax/03-nike-reactquery/src/App.jsx
@@ -1,38 +1,54 @@
-import { useCallback, useEffect, useState } from "react";
+import { /* useCallback, useEffect, */ useState } from "react";
 import Product from "./Product";
 import Shipping from "./Shipping";
 import { DotLoader } from "react-spinners";
 import useAxiosInstance from "@hooks/useAxiosInstance";
 import { Slide, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 function App() {
-  const [data, setData] = useState(); // 1(마운트)
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-
   const axios = useAxiosInstance();
 
-  const fetchData = async (_id) => {
-    setIsLoading(true);
-    try {
-      // const res = await axios.get(`/products/${_id}`, { params: { delay: 1000 } });
-      const res = await axios.get(`/products/${_id}`);
-      // console.log("res", res); // ('axios 응답 인터셉터 추가' 하기 위해선 이 부분은 삭제 해야함.)
-      setData(res.data.item); // 4번 (마운트 후)
-      setError(null);
-    } catch (err) {
-      // network 에러, 4xx, 5xx 응답일 경우
-      setError(err);
-      setData(null);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+  // const [data, setData] = useState(); // 1(마운트)
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(null);
 
-  useEffect(() => {
-    fetchData(7); // 3번(마운트 후)
-  }, []); // 마운트 된 이후에 최초 한번만 실행
+  // const fetchData = async (_id) => {
+  //   setIsLoading(true);
+  //   try{
+  //     // const res = await axios.get(`/products/${_id}`, { params: { delay: 1000 } });
+  //     const res = await axios.get(`/products/${_id}`);
+  //     setData(res.data.item); // 4번(마운트 후)
+  //     setError(null);
+  //   }catch(err){ // network 에러, 4xx, 5xx 응답일 경우
+  //     setError(err);
+  //     setData(null);
+  //   }finally{
+  //     setIsLoading(false);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   fetchData(7); // 3번(마운트 후)
+  // }, []); // 마운트 된 이후에 최초 한번만 실행
+
+  // 상품 상세 조회
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["상품상세조회번호는두번째요소에", 7], // 캐시에 사용할 키값을 지정(7번 상품)
+    queryFn: () => axios.get(`/products/7`), // 서버에 ajax 요청 코드(Promise 반환)
+    select: (res) => res.data.item,
+  });
+
+  // 상품 구매
+  const orderProduct = useMutation({
+    // useMutation() 반환한 객체의 mutate() 호출하면 mutationFn 호출됨
+    mutationFn: (products) => axios.post(`/orders`, products),
+  });
+
+  console.log("isLoading", isLoading);
+  console.log("error", error);
+  console.log("data", data);
 
   const basicShippingFees = 3000;
 
@@ -46,23 +62,33 @@ function App() {
     setQuantity(newQuantity);
   };
 
-  const handlePayment = useCallback(() => {
-    alert(`배송비 ${shippingFees}원이 추가됩니다. 상품을 결제하시겠습니까?`);
-  }, [shippingFees]);
+  const handlePayment = () => {
+    const ok = confirm(
+      `배송비 ${shippingFees}원이 추가됩니다. 상품을 결제하시겠습니까?`
+    );
+    if (ok) {
+      // mutateFn() 호출
+      orderProduct.mutate({
+        products: [{ _id: 7, quantity }],
+      });
+    }
+  };
 
   // return <h1></h1> // 2번(마운트)
   return (
     <>
-      <h1>02 Nike 상품 상세 조회 - Axios</h1>
+      <h1>03 Nike 상품 상세 조회 - React Query</h1>
       {isLoading && <DotLoader />}
-      {/* {error && <p>{error.message}</p>} */}
+      {error && <p>{error.message}</p>}
       {data && (
         <div>
           <Product product={data} />
           <h2>수량 선택</h2>
           <div>
             가격: {data.price.toLocaleString()}원<br />
-            수량:
+            남은 수량: {data.quantity - data.buyQuantity}
+            <br />
+            수량:{" "}
             <input
               type="number"
               min="1"
