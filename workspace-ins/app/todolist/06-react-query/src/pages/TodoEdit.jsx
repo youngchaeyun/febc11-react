009b2d59054ad4f61003bbc53d78af4f742a3b50
Kilyong Jeong <uzoolove@gmail.com>
2024-12-05 10:22:10 +0900

    👔 react-query로 수정

--- workspace-ins/app/todolist/06-react-query/src/pages/TodoEdit.jsx
+++ workspace-ins/app/todolist/06-react-query/src/pages/TodoEdit.jsx
@@ -1,11 +1,12 @@
 import useAxiosInstance from "@hooks/useAxiosInstance";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
 import { Link, useNavigate, useOutletContext } from "react-router-dom";
 
 function TodoEdit() {
 
   // Outlet 컴포넌트의 context 속성에 전달되는 값 추출
-  const { item, refetch } = useOutletContext();
+  const { item } = useOutletContext();
 
   // 프로그래밍 방식으로 페이지 이동에 사용
   const navigate = useNavigate();
@@ -20,28 +21,40 @@ function TodoEdit() {
 
   // axios 인스턴스
   const axios = useAxiosInstance();
+  const queryClient = useQueryClient();
 
-  // 수정 작업
-  const onSubmit = async (formData) => {
-    try{
-      // TODO: API 서버에 수정 요청
-      await axios.patch(`/todolist/${ item._id }`, formData);
+  const editItem = useMutation({
+    mutationFn: (formData) => axios.patch(`/todolist/${ item._id }`, formData),
+    onSuccess: () => {
       alert('할일이 수정 되었습니다.');
-
       // 할일 상세보기로 이동
       navigate(-1);
-      refetch();
-    }catch(err){
-      console.error(err);
-      alert('할일 수정에 실패했습니다.');
-    }
-  };
+      queryClient.invalidateQueries(['todolist', item._id]);
+    },
+    onError: () => {},
+  });
+
+  // 수정 작업
+  // const onSubmit = async (formData) => {
+  //   try{
+  //     // TODO: API 서버에 수정 요청
+  //     await axios.patch(`/todolist/${ item._id }`, formData);
+  //     alert('할일이 수정 되었습니다.');
+
+  //     // 할일 상세보기로 이동
+  //     navigate(-1);
+  //     refetch();
+  //   }catch(err){
+  //     console.error(err);
+  //     alert('할일 수정에 실패했습니다.');
+  //   }
+  // };
 
   return (
     <>
       <h2>할일 수정</h2>
       <div className="todo">
-        <form onSubmit={ handleSubmit(onSubmit) }>
+        <form onSubmit={ handleSubmit(editItem.mutate) }>
           <label htmlFor="title">제목 :</label>
           <input 
             type="text" 
