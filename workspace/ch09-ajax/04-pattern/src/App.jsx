d3f317d52c78eb56d82a871fb574be97db1874f1
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-12-05 15:10:55 +0900

    ✨ 2024년 12월 05일 ch09-ajax-04-pattern-src App.jsx (Fetch-on-render 패턴 개발 중)

--- workspace/ch09-ajax/04-pattern/src/App.jsx
+++ workspace/ch09-ajax/04-pattern/src/App.jsx
@@ -1,5 +1,12 @@
+import PostList from "./PostList";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>04 데이터 패칭 패턴 - Fetch-on-render, Fetch-then-render</h1>
+      <PostList />
+    </>
+  );
 }
 
 export default App;
