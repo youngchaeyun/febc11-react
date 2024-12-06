3c2f706e4ec6d77c37fb49eeba379260eb479344
Kilyong Jeong <uzoolove@gmail.com>
2024-12-06 14:32:57 +0900

    👔 컴포넌트 분리

--- workspace-ins/ch11-skeleton/lion-board/src/components/layout/index.jsx
+++ workspace-ins/ch11-skeleton/lion-board/src/components/layout/index.jsx
@@ -5,9 +5,13 @@ import { Outlet } from "react-router-dom";
 export default function Layout() {
   return (
     <div className="flex flex-col min-h-screen dark:bg-gray-700 dark:text-gray-200 transition-color duration-500 ease-in-out">
+
       <Header />
+
       <Outlet />
+      
       <Footer />
+      
     </div>
   );
 }
--- workspace-ins/ch11-skeleton/lion-board/src/pages/board/Detail.jsx
+++ workspace-ins/ch11-skeleton/lion-board/src/pages/board/Detail.jsx
@@ -1,3 +1,4 @@
+import CommentList from "@pages/board/CommentList";
 import { Link } from "react-router-dom";
 
 export default function Detail() {
@@ -22,67 +23,7 @@ export default function Detail() {
         </form>
       </section>
       
-      <section className="mb-8">
-        <h4 className="mt-8 mb-4 ml-2">댓글 2개</h4>
-
-        <div className="shadow-md rounded-lg p-4 mb-4">
-          <div className="flex justify-between items-center mb-2">
-            <img
-              className="w-8 mr-2 rounded-full"
-              src="https://api.fesp.shop/files/00-sample/user-muzi.webp"
-              alt="어피치 프로필 이미지"
-            />
-            <Link to="" className="text-orange-400">어피치</Link>
-            <time className="ml-auto text-gray-500" dateTime="2024.07.02 14:11:22">2024.07.02 14:11:22</time>
-          </div>
-          <div className="flex justify-between items-center mb-2">
-            <form action="#">
-              <pre className="whitespace-pre-wrap text-sm">화이팅!</pre>
-              <button type="submit" className="bg-red-500 py-1 px-2 text-sm text-white font-semibold ml-2 hover:bg-amber-400 rounded">삭제</button>
-            </form>
-          </div>  
-        </div>
-
-        <div className="shadow-md rounded-lg p-4 mb-4">
-          <div className="flex justify-between items-center mb-2">
-            <img
-              className="w-8 mr-2 rounded-full"
-              src="https://api.fesp.shop/files/00-sample/user-muzi.webp"
-              alt="무지 프로필 이미지"
-            />
-            <Link to="" className="text-orange-400">무지</Link>
-            <time className="ml-auto text-gray-500" dateTime="2024.07.07 12:34:56">2024.07.07 12:34:56</time>
-          </div>
-          <div className="flex justify-between items-center mb-2">
-            <form action="#">
-              <pre className="whitespace-pre-wrap text-sm">축하해요~~~</pre>
-              <button type="submit" className="bg-red-500 py-1 px-2 text-sm text-white font-semibold ml-2 hover:bg-amber-400 rounded">삭제</button>
-            </form>
-          </div>  
-        </div>
-
-        <div className="p-4 border border-gray-200 rounded-lg">
-          <h4 className="mb-4">새로운 댓글을 추가하세요.</h4>
-          <form action="#">
-            <div className="mb-4">
-              <textarea
-                rows="3"
-                cols="40"
-                className="block p-2 w-full text-sm border rounded-lg border-gray-300 bg-gray-50 focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
-                placeholder="내용을 입력하세요."
-                name="comment"></textarea>
-    
-              <p className="ml-2 mt-1 text-sm text-red-500">
-                내용은 필수입니다.
-              </p>
-              
-            </div>
-            <button type="submit" className="bg-orange-500 py-1 px-4 text-sm text-white font-semibold ml-2 hover:bg-amber-400 rounded">댓글 등록</button>
-          </form>
-        </div>
-  
-      </section>
-
+      <CommentList />
 
     </main>
   );
