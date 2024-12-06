70b4e8f4bc41a82b77d12c792abc35c8f96850f3
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-12-06 15:29:33 +0900

    👔 2024년 12월 06일 (컴포넌트 분리) 파일 수정과 새로운 파일 추가

--- workspace/ch11-skeleton/lion-board/src/pages/board/List.jsx
+++ workspace/ch11-skeleton/lion-board/src/pages/board/List.jsx
@@ -1,3 +1,4 @@
+import ListItem from "@pages/board/ListItem";
 import { Link } from "react-router-dom";
 
 export default function List() {
@@ -57,34 +58,7 @@ export default function List() {
             </tr>
           </thead>
           <tbody>
-            <tr className="border-b border-gray-200 hover:bg-gray-200 dark:hover:bg-gray-700 transition duration-300 ease-in-out">
-              <td className="p-2 text-center">2</td>
-              <td className="p-2 truncate indent-4">
-                <Link to="/info/2" className="cursor-pointer">
-                  안녕하세요.
-                </Link>
-              </td>
-              <td className="p-2 text-center truncate">용쌤</td>
-              <td className="p-2 text-center hidden sm:table-cell">29</td>
-              <td className="p-2 text-center hidden sm:table-cell">2</td>
-              <td className="p-2 truncate text-center hidden sm:table-cell">
-                2024.07.05 13:39:23
-              </td>
-            </tr>
-            <tr className="border-b border-gray-200 hover:bg-gray-200 dark:hover:bg-gray-700 transition duration-300 ease-in-out">
-              <td className="p-2 text-center">1</td>
-              <td className="p-2 truncate indent-4">
-                <Link to="/info/1" className="cursor-pointer">
-                  좋은 소식이 있습니다.
-                </Link>
-              </td>
-              <td className="p-2 text-center truncate">제이지</td>
-              <td className="p-2 text-center hidden sm:table-cell">22</td>
-              <td className="p-2 text-center hidden sm:table-cell">5</td>
-              <td className="p-2 truncate text-center hidden sm:table-cell">
-                2024.07.03 17:59:13
-              </td>
-            </tr>
+            <ListItem />
           </tbody>
         </table>
         <hr />
