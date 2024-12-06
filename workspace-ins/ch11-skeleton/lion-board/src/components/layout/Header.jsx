3eaff2b73dd0e130b6d984bd37cba62616233ac2
Kilyong Jeong <uzoolove@gmail.com>
2024-12-06 13:21:12 +0900

    👔 a -> Link로 수정

--- workspace-ins/ch11-skeleton/lion-board/src/components/layout/Header.jsx
+++ workspace-ins/ch11-skeleton/lion-board/src/components/layout/Header.jsx
@@ -1,26 +1,28 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
     <header className="px-8 min-w-80 bg-slate-100 dark:bg-gray-600 text-gray-800 dark:text-gray-200 transition-color duration-500 ease-in-out">
       <nav className="flex flex-wrap justify-center items-center p-4 md:flex-nowrap md:justify-between">
         <div className="w-1/2 order-1 md:w-auto">
-          <a href="/" className="flex items-center gap-2">
+          <Link to="/" className="flex items-center gap-2">
             <img className="mr-3 h-6 sm:h-9" src="/images/favicon.svg" width="40" height="40" alt="로고 이미지" />
             <span className="text-lg font-bold">멋사컴</span>
-          </a>
+          </Link>
         </div>
         <div className="w-auto order-2 text-base mt-4 md:mt-0">
           <ul className="flex items-center gap-6 uppercase">
-            <li className="hover:text-amber-500 hover:font-semibold"><a href="/info">정보공유</a></li>
-            <li className="hover:text-amber-500 hover:font-semibold"><a href="/free">자유게시판</a></li>
-            <li className="hover:text-amber-500 a:font-semibold"><a href="/qna">질문게시판</a></li>
+            <li className="hover:text-amber-500 hover:font-semibold"><Link to="/info">정보공유</Link></li>
+            <li className="hover:text-amber-500 hover:font-semibold"><Link to="/free">자유게시판</Link></li>
+            <li className="hover:text-amber-500 a:font-semibold"><Link to="/qna">질문게시판</Link></li>
           </ul>
         </div>
 
         <div className="w-1/2 order-1 flex justify-end items-center md:order-2 md:w-auto">
 
           <div className="flex justify-end">
-            <a href="/user/login" className="bg-orange-500 py-1 px-2 text-sm text-white font-semibold ml-2 hover:bg-amber-400 rounded">로그인</a>
-            <a href="/user/signup" className="bg-gray-900 py-1 px-2 text-sm text-white font-semibold ml-2 hover:bg-amber-400 rounded">회원가입</a>
+            <Link to="/users/login" className="bg-orange-500 py-1 px-2 text-sm text-white font-semibold ml-2 hover:bg-amber-400 rounded">로그인</Link>
+            <Link to="/users/signup" className="bg-gray-900 py-1 px-2 text-sm text-white font-semibold ml-2 hover:bg-amber-400 rounded">회원가입</Link>
           </div>
 
           <button
