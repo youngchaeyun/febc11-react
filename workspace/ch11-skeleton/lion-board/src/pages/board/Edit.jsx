f16484245d2a5e6aa71f174dc0de7c33871714db
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-12-09 17:06:50 +0900

    ✨ 2024년 12월 09일 ch11-skeleton-lion-board-src-pages-board-Edit.jsx (게시글 수정)

--- workspace/ch11-skeleton/lion-board/src/pages/board/Edit.jsx
+++ workspace/ch11-skeleton/lion-board/src/pages/board/Edit.jsx
@@ -1,6 +1,47 @@
-import { Link } from "react-router-dom";
+import InputError from "@components/InputError";
+import useAxiosInstance from "@hooks/useAxiosInstance";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 export default function Edit() {
+  const { type, _id } = useParams();
+  const axios = useAxiosInstance();
+
+  const { data } = useQuery({
+    queryKey: ["posts", _id],
+    queryFn: () => axios.get(`/posts/${_id}`),
+    select: (res) => res.data,
+    staleTime: 1000 * 10,
+  });
+
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      title: data?.item.title,
+      content: data?.item.content,
+    },
+  });
+
+  const queryClient = useQueryClient();
+
+  const updateItem = useMutation({
+    mutationFn: (formData) => axios.patch(`/posts/${_id}`, formData),
+    onSuccess: () => {
+      alert("게시물이 수정되었습니다.");
+      queryClient.invalidateQueries(["posts", _id]);
+      navigate(`/${type}/${_id}`);
+    },
+    onError: (err) => {
+      console.error(err);
+    },
+  });
+
   return (
     <main className="min-w-[320px] p-4">
       <div className="text-center py-4">
@@ -9,7 +50,7 @@ export default function Edit() {
         </h2>
       </div>
       <section className="mb-8 p-4">
-        <form action="/info/1">
+        <form onSubmit={handleSubmit(updateItem.mutate)}>
           <div className="my-4">
             <label className="block text-lg content-center" htmlFor="title">
               제목
@@ -19,13 +60,9 @@ export default function Edit() {
               type="text"
               placeholder="제목을 입력하세요."
               className="w-full py-2 px-4 border rounded-md dark:bg-gray-700 border-gray-300 focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200"
-              name="title"
-              value="좋은 소식이 있습니다."
+              {...register("title", { required: "제목은 필수입니다." })}
             />
-
-            <p className="ml-2 mt-1 text-sm text-red-500 dark:text-red-400">
-              제목은 필수 입니다.
-            </p>
+            <InputError target={errors.title} />
           </div>
           <div className="my-4">
             <label className="block text-lg content-center" htmlFor="content">
@@ -36,15 +73,9 @@ export default function Edit() {
               rows="15"
               placeholder="내용을 입력하세요."
               className="w-full p-4 text-sm border rounded-lg border-gray-300 bg-gray-50 focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
-              name="content"
-            >
-              좋은 소식을 가지고 왔습니다. 오늘 드디어 최종 면접을 합니다. 많이
-              응원해 주세요^^
-            </textarea>
-
-            <p className="ml-2 mt-1 text-sm text-red-500 dark:text-red-400">
-              내용은 필수입니다.
-            </p>
+              {...register("content", { required: "내용은 필수입니다." })}
+            />
+            <InputError target={errors.content} />
           </div>
           <hr />
           <div className="flex justify-end my-6">
@@ -55,7 +86,7 @@ export default function Edit() {
               수정
             </button>
             <Link
-              to="/info/1"
+              to={`/${type}/${_id}`}
               className="bg-gray-900 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded"
             >
               취소
