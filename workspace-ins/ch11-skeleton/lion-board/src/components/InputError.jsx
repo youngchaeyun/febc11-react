cf58858086a1e46ca6304566b0f183b37687291b
Kilyong Jeong <uzoolove@gmail.com>
2024-12-09 13:06:11 +0900

    ♻️ PropTypes 추가

--- workspace-ins/ch11-skeleton/lion-board/src/components/InputError.jsx
+++ workspace-ins/ch11-skeleton/lion-board/src/components/InputError.jsx
@@ -1,3 +1,9 @@
+import PropTypes from "prop-types";
+
+InputError.propTypes = {
+  target: PropTypes.object.isRequired,
+};
+
 export default function InputError({ target }) {
   if(!target) return;
   return <p className="ml-2 mt-1 text-sm text-red-500 dark:text-red-400">{ target.message }</p>;
