552e1c0d25bb1e16aa616faa9f2788bdc7281b82
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-12-09 13:32:30 +0900

    ✨ 2024년 12월 09일 (게시물 상세조회 완성)

--- workspace/ch11-skeleton/lion-board/src/pages/board/ListItem.jsx
+++ workspace/ch11-skeleton/lion-board/src/pages/board/ListItem.jsx
@@ -19,7 +19,7 @@ export default function ListItem({ item }) {
     <tr className="border-b border-gray-200 hover:bg-gray-200 dark:hover:bg-gray-700 transition duration-300 ease-in-out">
       <td className="p-2 text-center">{item._id}</td>
       <td className="p-2 truncate indent-4">
-        <Link to="/info/2" className="cursor-pointer">
+        <Link to={`${item._id}`} className="cursor-pointer">
           {item.title}
         </Link>
       </td>
