e26d5f6926bb5c5f77f66c673397b152f08b6e92
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-12-09 11:31:13 +0900

    ♻️ 2024년 12월 06일 (자잘한 경고 수정)

--- workspace/ch11-skeleton/lion-board-step1/src/App.jsx
+++ workspace/ch11-skeleton/lion-board-step1/src/App.jsx
@@ -1,10 +1,10 @@
 import { RouterProvider } from "react-router-dom";
-import router from '@/routes';
+import router from "@/routes";
 
 function App() {
   return (
-    <RouterProvider router={ router } />
+    <RouterProvider router={router} future={{ v7_startTransition: true }} />
   );
 }
 
-export default App
+export default App;
