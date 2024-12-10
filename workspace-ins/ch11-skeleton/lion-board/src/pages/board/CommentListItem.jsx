1c0ccf4456a9ed6569c7a7df484f9163242f43cf
Kilyong Jeong <uzoolove@gmail.com>
2024-12-10 13:03:40 +0900

    ✨ 댓글 삭제

--- workspace-ins/ch11-skeleton/lion-board/src/pages/board/CommentListItem.jsx
+++ workspace-ins/ch11-skeleton/lion-board/src/pages/board/CommentListItem.jsx
@@ -1,5 +1,7 @@
+import useAxiosInstance from "@hooks/useAxiosInstance";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 CommentListItem.propTypes = {
   item: PropTypes.shape({
@@ -13,6 +15,18 @@ CommentListItem.propTypes = {
   }).isRequired,
 };
 export default function CommentListItem({ item }) {
+
+  const queryClient = useQueryClient();
+  const axios = useAxiosInstance();
+  const { _id } = useParams();
+
+  const removeItem = useMutation({
+    mutationFn: _id => axios.delete(`/posts/${_id}/replies/${item._id}`),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['posts', _id] });
+    }
+  });
+
   return (
     <div className="shadow-md rounded-lg p-4 mb-4">
       <div className="flex justify-between items-center mb-2">
@@ -25,10 +39,8 @@ export default function CommentListItem({ item }) {
         <time className="ml-auto text-gray-500" dateTime={`${item.createdAt}`}>{ item.createdAt }</time>
       </div>
       <div className="flex justify-between items-center mb-2">
-        <form action="#">
-          <pre className="whitespace-pre-wrap text-sm">{ item.content }</pre>
-          <button type="submit" className="bg-red-500 py-1 px-2 text-sm text-white font-semibold ml-2 hover:bg-amber-400 rounded">삭제</button>
-        </form>
+        <pre className="whitespace-pre-wrap text-sm">{ item.content }</pre>
+        <button type="button" onClick={ () => removeItem.mutate(_id) } className="bg-red-500 py-1 px-2 text-sm text-white font-semibold ml-2 hover:bg-amber-400 rounded">삭제</button>
       </div>
     </div>
   );
