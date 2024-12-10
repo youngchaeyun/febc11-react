df1b5d28b05b2986d0c99c163a75257226fb1167
Kilyong Jeong <uzoolove@gmail.com>
2024-12-10 11:19:26 +0900

    ✨ 게시글 삭제 기능 추가

--- workspace-ins/ch11-skeleton/lion-board/src/pages/board/Detail.jsx
+++ workspace-ins/ch11-skeleton/lion-board/src/pages/board/Detail.jsx
@@ -1,10 +1,10 @@
 import useAxiosInstance from "@hooks/useAxiosInstance";
 import CommentList from "@pages/board/CommentList";
-import { useQuery } from "@tanstack/react-query";
-import { Link, useParams } from "react-router-dom";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 export default function Detail() {
-
+  const navigate = useNavigate();
   const axios = useAxiosInstance();
   const { type, _id } = useParams();
 
@@ -15,7 +15,21 @@ export default function Detail() {
     staleTime: 1000*10,
   });
 
-  console.log(data);
+  const queryClient = useQueryClient();
+
+  const removeItem = useMutation({
+    mutationFn: _id => axios.delete(`/posts/${_id}`),
+    onSuccess: () => {
+      alert('게시물이 삭제되었습니다.');
+      queryClient.invalidateQueries({ queryKey: ['posts', type] });
+      navigate(`/${type}`);
+    }
+  });
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    removeItem.mutate(_id);
+  };
 
   if(!data){
     return <div>로딩중...</div>;
@@ -25,7 +39,7 @@ export default function Detail() {
     <main className="container mx-auto mt-4 px-4">
 
       <section className="mb-8 p-4">
-        <form action="/info">
+        <form onSubmit={ onSubmit }>
           <div className="font-semibold text-xl">제목 : { data.item.title }</div>
           <div className="text-right text-gray-400">작성자 : { data.item.user.name }</div>
           <div className="mb-4">
