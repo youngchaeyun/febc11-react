dc9f9de779e7647fe5ad94771aa15995c6544f2a
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-12-10 14:35:53 +0900

    ✨ 2024년 12월 10일 (회원의 프로필 이미지 추가)

--- workspace/ch11-skeleton/lion-board/src/pages/board/CommentListItem.jsx
+++ workspace/ch11-skeleton/lion-board/src/pages/board/CommentListItem.jsx
@@ -29,11 +29,13 @@ export default function CommentListItem({ item }) {
   return (
     <div className="shadow-md rounded-lg p-4 mb-4">
       <div className="flex justify-between items-center mb-2">
-        <img
-          className="w-8 mr-2 rounded-full"
-          src="https://api.fesp.shop/files/00-sample/user-muzi.webp"
-          alt={`${item.user.name} 프로필 이미지`}
-        />
+        {item.user.image && (
+          <img
+            className="w-8 mr-2 rounded-full"
+            src={`https://api.fesp.shop${item.user.image.path}`}
+            alt={`${item.user.name} 프로필 이미지`}
+          />
+        )}
         <Link to="" className="text-orange-400">
           {item.user.name || "익명"}
         </Link>
