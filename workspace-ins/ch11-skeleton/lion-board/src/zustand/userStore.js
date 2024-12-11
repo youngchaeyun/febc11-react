4fca91f6262e94db43b5057c83b7eb9550e29758
Kilyong Jeong <uzoolove@gmail.com>
2024-12-11 10:56:24 +0900

    ♻️ UserStore 정리

--- workspace-ins/ch11-skeleton/lion-board/src/zustand/userStore.js
+++ workspace-ins/ch11-skeleton/lion-board/src/zustand/userStore.js
@@ -1,13 +1,18 @@
 import { create } from "zustand";
 import { persist, createJSONStorage } from 'zustand/middleware';
 
-const useUserStore = create(persist((set) => ({
+const UserStore = (set) => ({
   user: null,
   setUser: (user) => set({ user }),
   resetUser: () => set({ user: null }),
-}), {
+});
+
+// const useUserStore = create(UserStore); // 스토리지를 사용하지 않을 경우
+
+// 스토리지를 사용할 경우
+const useUserStore = create(persist(UserStore), {
   name: 'user',
   storage: createJSONStorage(() => sessionStorage) // 기본은 localStorage
-}));
+});
 
 export default useUserStore;
