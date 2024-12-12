b11b80f9ed79bc4cbdaf1a0a6d315e9a983e5767
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-12-12 16:43:02 +0900

    ✨ 2024년 12월 12일 (step3 완성)

--- workspace/ch11-skeleton/lion-board-step3/src/routes.jsx
+++ workspace/ch11-skeleton/lion-board-step3/src/routes.jsx
@@ -7,21 +7,31 @@ import MainPage from "@pages/index";
 import Login from "@pages/user/Login";
 import Signup from "@pages/user/Signup";
 import { createBrowserRouter } from "react-router-dom";
-
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { index: true, element: <MainPage /> },
+        { path: ":type", element: <List /> },
+        { path: ":type/new", element: <New /> },
+        { path: ":type/:_id", element: <Detail /> },
+        { path: ":type/:_id/edit", element: <Edit /> },
+        { path: "users/signup", element: <Signup /> },
+        { path: "users/login", element: <Login /> },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <Layout />,
-    children: [
-      { index: true, element: <MainPage /> },
-      { path: ":type", element: <List /> },
-      { path: ":type/new", element: <New /> },
-      { path: ":type/:_id", element: <Detail /> },
-      { path: ":type/:_id/edit", element: <Edit /> },
-      { path: "users/signup", element: <Signup /> },
-      { path: "users/login", element: <Login /> },
-    ],
-  },
-]);
-
+    future: {
+      // 없으면 콘솔에 경고 표시
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_relativeSplatPath: true,
+      v7_skipActionErrorRevalidation: true,
+    },
+  }
+);
 export default router;
