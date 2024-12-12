cf16e96ab47ae2b24a2fa6e6dcb02ecf00f2c294
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-12-12 17:42:40 +0900

    ✨ 2024년 12월 12일 ch10-nextjs-01-manual 새로운 폴더(01-manual) 생성 완료! - (nextjs 의 시작, 준비 완료)

---
+++ workspace/ch10-nextjs/01-manual/app/layout.jsx
@@ -0,0 +1,10 @@
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <head>
+        <title>Next.js App</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ workspace/ch10-nextjs/01-manual/app/page.jsx
@@ -0,0 +1,5 @@
+export default function MainPage() {
+  console.log("MainPage 호출됨...");
+  // for(;;){}
+  return <h1>Hello Next.js</h1>;
+}
