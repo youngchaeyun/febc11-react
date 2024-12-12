6d0bc939cffe05d4ee0348684a1af7cadd9224bc
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-12-12 17:06:12 +0900

    ✨ 2024년 12월 12일 package-lock.json 과 App.jsx 와 Detail.jsx 파일 (helmet 추가)

--- workspace/ch11-skeleton/lion-board/src/App.jsx
+++ workspace/ch11-skeleton/lion-board/src/App.jsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import router from "@/routes";
 import useThemeStore from "@zustand/themeStore";
+import { HelmetProvider } from "react-helmet-async";
 
 function App() {
   const { isDarkMode } = useThemeStore();
@@ -12,7 +13,9 @@ function App() {
   }
 
   return (
-    <RouterProvider router={router} future={{ v7_startTransition: true }} />
+    <HelmetProvider>
+      <RouterProvider router={router} future={{ v7_startTransition: true }} />
+    </HelmetProvider>
   );
 }
 
