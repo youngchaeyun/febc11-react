b08a5fc4ea3e6214ba55d5767114921b09dc770c
Kilyong Jeong <uzoolove@gmail.com>
2024-12-12 11:28:43 +0900

    ✨ helmet 추가

--- workspace-ins/ch11-skeleton/lion-board/src/App.jsx
+++ workspace-ins/ch11-skeleton/lion-board/src/App.jsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import router from '@/routes';
 import useThemeStore from "@zustand/themeStore";
+import { HelmetProvider } from "react-helmet-async";
 
 function App() {
 
@@ -13,7 +14,9 @@ function App() {
   }
   
   return (
-    <RouterProvider router={router} future={{ v7_startTransition: true }} />
+    <HelmetProvider>
+      <RouterProvider router={router} future={{ v7_startTransition: true }} />
+    </HelmetProvider>
   );
 }
 
--- workspace-ins/ch11-skeleton/lion-board/src/pages/board/Detail.jsx
+++ workspace-ins/ch11-skeleton/lion-board/src/pages/board/Detail.jsx
@@ -2,6 +2,7 @@ import useAxiosInstance from "@hooks/useAxiosInstance";
 import CommentList from "@pages/board/CommentList";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import useUserStore from "@zustand/userStore";
+import { Helmet } from "react-helmet-async";
 import { Link, useNavigate, useParams } from "react-router-dom";
 
 export default function Detail() {
@@ -40,34 +41,44 @@ export default function Detail() {
   }
 
   return (
-    <main className="container mx-auto mt-4 px-4">
 
-      <section className="mb-8 p-4">
-        <form onSubmit={ onSubmit }>
-          <div className="font-semibold text-xl">제목 : { data.item.title }</div>
-          <div className="text-right text-gray-400">작성자 : { data.item.user.name }</div>
-          <div className="mb-4">
-            <div>
-              <pre className="font-roboto w-full p-2 whitespace-pre-wrap">{ data.item.content }</pre>
+    <>
+      <Helmet>
+        <title>{ data.item.title } - 멋사컴</title>
+        <meta property="og:title" content={ data.item.title } />
+        <meta property="og:description" content={ data.item.content } />
+      </Helmet>
+
+      <main className="container mx-auto mt-4 px-4">
+
+        <section className="mb-8 p-4">
+          <form onSubmit={ onSubmit }>
+            <div className="font-semibold text-xl">제목 : { data.item.title }</div>
+            <div className="text-right text-gray-400">작성자 : { data.item.user.name }</div>
+            <div className="mb-4">
+              <div>
+                <pre className="font-roboto w-full p-2 whitespace-pre-wrap">{ data.item.content }</pre>
+              </div>
+              <hr/>
             </div>
-            <hr/>
-          </div>
-          <div className="flex justify-end my-4">
-            <Link to={`/${type}`} className="bg-orange-500 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded">목록</Link>
+            <div className="flex justify-end my-4">
+              <Link to={`/${type}`} className="bg-orange-500 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded">목록</Link>
 
-            { user?._id === data.item.user._id && (
-              <>
-                <Link to={`/${type}/${_id}/edit`} className="bg-gray-900 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded">수정</Link>
-                <button type="submit" className="bg-red-500 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded">삭제</button>
-              </>
-            ) }
+              { user?._id === data.item.user._id && (
+                <>
+                  <Link to={`/${type}/${_id}/edit`} className="bg-gray-900 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded">수정</Link>
+                  <button type="submit" className="bg-red-500 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded">삭제</button>
+                </>
+              ) }
 
-          </div>
-        </form>
-      </section>
-      
-      <CommentList data={ data.item.replies } />
+            </div>
+          </form>
+        </section>
 
-    </main>
+        <CommentList data={ data.item.replies } />
+
+      </main>
+    </>
+    
   );
 }
