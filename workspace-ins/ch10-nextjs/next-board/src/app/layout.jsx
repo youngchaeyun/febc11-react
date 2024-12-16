cc8986d220e915620a33912b1d1a91074f9d028c
Kilyong Jeong <uzoolove@gmail.com>
2024-12-16 11:05:38 +0900

    👔 dynamic 라우팅 적용용

--- workspace-ins/ch10-nextjs/next-board/src/app/layout.jsx
+++ workspace-ins/ch10-nextjs/next-board/src/app/layout.jsx
@@ -1,3 +1,5 @@
+import "./globals.css";
+
 import Link from "next/link";
 
 export default function RootLayout({ children }) {
@@ -19,8 +21,6 @@ export default function RootLayout({ children }) {
         <meta property="og:url" content="https://board.fesp.shop" />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="멋사컴" />
-
-        <script src="https://cdn.tailwindcss.com"></script>
 
       </head>
       <body>
@@ -80,7 +80,9 @@ export default function RootLayout({ children }) {
                 </div>
               </nav>
             </header>
-            { children }            
+
+            { children }          
+              
             <footer className="p-4 pb-12 w-full border-t border-t-slate-200  dark:border-t-slate-500 dark:bg-gray-600 text-gray-600 dark:text-white transition-color duration-500 ease-in-out">
               <div className="min-w-[320px] flex flex-wrap gap-4 justify-center items-center text-sm text-slate-400">
                 <Link href="#" className="hover:font-semibold dark:hover:text-gray-200">약관</Link>
