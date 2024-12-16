34dafa8360c25f496a2b05d9bd1a1d91320f014b
Kilyong Jeong <uzoolove@gmail.com>
2024-12-16 13:24:07 +0900

    ✨ 게시물 상세조회 AIP 연동 완료

--- workspace-ins/ch10-nextjs/next-board/src/app/[type]/[_id]/page.jsx
+++ workspace-ins/ch10-nextjs/next-board/src/app/[type]/[_id]/page.jsx
@@ -1,17 +1,30 @@
 import Link from "next/link";
 
-export default function Page() {
+// 게시물 상세정보를 조회해서 반환
+async function fetchPost(_id){
+  const url = `https://11.fesp.shop/posts/${_id}`;
+  const res = await fetch(url, {
+    headers: { 'client-id': '00-board' }
+  });
+  return await res.json();
+}
+
+export default async function Page({ params }) {
+  const { _id } = await params;
+
+  const data = await fetchPost(_id);
+
   return (
     <>
       <main className="container mx-auto mt-4 px-4">
 
         <section className="mb-8 p-4">
           <form action="/info">
-          <div className="font-semibold text-xl">제목 : 좋은 소식이 있습니다.</div>
-            <div className="text-right text-gray-400">작성자 : 제이지</div>
+          <div className="font-semibold text-xl">제목 : { data.item.title }</div>
+            <div className="text-right text-gray-400">작성자 : { data.item.user.name }</div>
             <div className="mb-4">
               <div>
-                <pre className="font-roboto w-full p-2 whitespace-pre-wrap">좋은 소식을 가지고 왔습니다.<br />오늘 드디어 최종 면접을 합니다.<br />많이 응원해 주세요^^</pre>
+                <pre className="font-roboto w-full p-2 whitespace-pre-wrap">{ data.item.content }</pre>
               </div>
               <hr/>
             </div>
