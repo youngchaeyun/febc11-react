716869832a7dd282878dffc32bc7fff28134856b
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-12-16 13:46:59 +0900

    ✨ next-board-src-app-error.jsx (에러 메세지 출력)

--- workspace/ch10-nextjs/next-board/src/app/error.jsx
+++ workspace/ch10-nextjs/next-board/src/app/error.jsx
@@ -1,13 +1,14 @@
 "use client";
 
-export default function Error() {
+export default function Error({ error, reset }) {
+  console.error(error);
   return (
     <div className="py-20 bg-red-100 border border-red-400 text-red-700 p-4 rounded-lg flex flex-col items-center space-y-2">
       <h2 className="text-xl font-semibold mb-2 text-center">
         🚧 앗, 무언가 잘못됐네요!
       </h2>
       <h3 className="text-md font-semibold mb-2 text-center">
-        일시적인 오류가 발생했습니다.
+        {error.message}
       </h3>
       <p className="pt-12 text-center">
         이 오류는 더 나은 서비스를 위한 첫걸음이에요. 조금만 기다려 주세요!
